83f5e5f2184b0ab05c2bf9ff14922a50370dbff5
CJIAsusLabtop <3000mn@naver.com>
2022-10-25 21:31:32 +0900

    장애물 벽 숨겨짐 효과
    장애물 벽과 충돌시 숨겨짐 효과 해제

--- WallObject.py
+++ WallObject.py
@@ -22,23 +22,23 @@ class WALL:
         # 화면에 그려질지 말지 플레이어와 부딫히면 그려짐.
         self.status = False
     def Draw(self):
-        if not self.status:
+        if self.status:
             self.image.draw(self.x,self.y)
     def Crash(self,player,index):
         # 플레이어가 벽의 사각형 내에 있을때 (총 4개의 점)
         if (player.x1 > self.x1 and player.x1 < self.x2 and # 좌
-        (player.y1+player.y2)//2 < self.y1 and (player.y1+player.y2)//2 > self.y2 and
-        PlayerObject.xPos < 0):
-            player.WallCrash()
-            print("충돌")
+        (player.y1+player.y2)//2 < self.y1 and (player.y1+player.y2)//2 > self.y2):
+            if PlayerObject.xPos < 0 :
+                player.WallCrash(); self.status = True
+                print("충돌")
             elif PlayerObject.xPos == 0 :
                 player.x += 1; player.x1 += 1 ; player.x2 += 1
                 print("충돌")
         elif (player.x2 > self.x1 and player.x2 < self.x2 and # 우
-        (player.y1+player.y2)//2 < self.y1 and (player.y1+player.y2)//2 > self.y2 and
-        PlayerObject.xPos > 0):
-            player.WallCrash()
-            print("충돌")
+        (player.y1+player.y2)//2 < self.y1 and (player.y1+player.y2)//2 > self.y2):
+            if PlayerObject.xPos > 0 :
+                player.WallCrash(); self.status = True
+                print("충돌")
             elif PlayerObject.xPos == 0 :
                 player.x -= 1; player.x1 -= 1 ; player.x2 -= 1
                 print("충돌")
@@ -49,10 +49,11 @@ class WALL:
             elif (self.x1 + self.x2)//2 < (player.x2+player.x1)//2:
                 player.x += 5; player.x1 += 5 ; player.x2 += 5
         elif ((player.x1+player.x2)//2 > self.x1 and (player.x1+player.x2)//2 < self.x2 and # 상
-        player.y1 < self.y1 and player.y1 > self.y2 ):
+        player.y1 < self.y1 and player.y1 > self.y2):
             if PlayerObject.yPos != 0:
                 player.y -= PlayerObject.yPos ; player.y1 -= PlayerObject.yPos ;player.y2 -= PlayerObject.yPos
             print("충돌")
+            self.status = True
         elif ((player.x1+player.x2)//2 > self.x1 and (player.x1+player.x2)//2 < self.x2 and # 하
         player.y2 < self.y1 and player.y2 > self.y2 and PlayerObject.FALLING):
             PlayerObject.JUMPKEYDOWN = False
@@ -61,6 +62,7 @@ class WALL:
             player.y1 = player.y + player.Right_Idle.h//2
             player.Wallpoint = index
             print("충돌")
+            self.status = True
 
 
 def SizeOfWall():
